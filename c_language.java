11a005dc06a6e41d1b03df6cb0d57be80d4a3322
Pranavkr6200 <147341721+Pranavkr6200@users.noreply.github.com>
2024-02-01 22:14:01 +0530

    Update c_language.java

--- c_language.java
+++ c_language.java
@@ -1,7 +1,7 @@
 public class c_language {
     public static void main(String[] args) {
         System.out.println("hello pranav kumar ");
-        System.out.println("vaishnavi shukla loves you❤️");
+        System.out.println("God loves you❤️");
         System.out.println("aisa mujhe lagta hai but i'm wrong😔");
 
     }
